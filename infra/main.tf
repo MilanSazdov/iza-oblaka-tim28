a6f21c2e4fd499ca844e8bd399a6a9ab2fc60da3
vedranbajic4 <bajic196@gmail.com>
2026-05-29 13:52:46 +0200

    add: main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -7,3 +7,37 @@
 # - module "s3"      { source = "./modules/global/s3" ... }
 # - module "vpc"     { source = "./modules/eu-central-1/vpc" ... }
 # - module "lambdas" { source = "./modules/eu-central-1/lambdas" ... wiring iam role arns, bucket arns, subnet ids }
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "global_iam" {
+  source      = "./modules/global/iam"
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+module "global_s3" {
+  source       = "./modules/global/s3"
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+module "eu_vpc" {
+  source       = "./modules/eu-central-1/vpc"
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+module "eu_lambdas" {
+  source       = "./modules/eu-central-1/lambdas"
+  project_name = var.project_name
+  environment  = var.environment
+
+  # Inputs from other modules:
+  bronze_bucket_arn = module.global_s3.bronze_bucket_arn
+  bronze_bucket_name = module.global_s3.bronze_bucket_name
+
+  lambda_execution_role_arn = module.global_iam.lambda_execution_role_arn
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,3 +6,10 @@
 # - output "gold_bucket_name"      { value = module.s3.gold_bucket_name }
 # - output "lambda_role_arn"       { value = module.iam.lambda_role_arn }
 # - output "vpc_id"                { value = module.vpc.vpc_id }
+
+# TODO: rename bronze bucket name
+
+output "bronze_bucket_name" {
+  description = "Bronze data lake bucket"
+  value       = module.global_s3.bronze_bucket_name
+}
