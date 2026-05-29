1a3fc340b8a0cb02ed9993abafede65aab6bb583
vedranbajic4 <bajic196@gmail.com>
2026-05-29 13:51:03 +0200

    add: variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,3 +5,20 @@
 # - variable "aws_region"  { default = "eu-central-1" }
 # - variable "project_name" { default = "iza-oblaka-tim28" }
 # - any other shared inputs (tags, account id, etc.)
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-central-1"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev, prod, ...)"
+  default     = "dev"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project base name"
+  default     = "iza-oblaka-tim28"
+}
